73f59991d86194c87f3ce35e1f01f8e3b1190738
doncsugar <46570709+doncsugar@users.noreply.github.com>
2024-02-17 11:24:48 -0500

    fixed structure for dynamic wallpaper

--- wallpaper-themes/genthemes.sh
+++ wallpaper-themes/genthemes.sh
@@ -5,15 +5,15 @@ mkdir output
 
 echo "Making sumac-wallpapers"
 mkdir output/sumac-wallpapers
-mkdir -p output/sumac-wallpapers/src/images
-mkdir -p output/sumac-wallpapers/images
-mkdir -p output/sumac-wallpapers/images_dark
+mkdir -p output/sumac-wallpapers/contents/src/images
+mkdir -p output/sumac-wallpapers/contents/images
+mkdir -p output/sumac-wallpapers/contents/images_dark
 
 cp -a sumac-wallpapers/* output/sumac-wallpapers
-cp -a src/* output/sumac-wallpapers/src/images
-mv output/sumac-wallpapers/src/images/sumac-day-13.svg "output/sumac-wallpapers/src/images/Sumac Day 13.svg"
-mv output/sumac-wallpapers/src/images/sumac-night-13.svg "output/sumac-wallpapers/src/images/Sumac Night 13.svg"
-ln -s "../src/images/Sumac Day 13.svg" output/sumac-wallpapers/images/6000x6000.svg
-ln -s "../src/images/Sumac Night 13.svg" output/sumac-wallpapers/images_dark/6000x6000.svg
+cp -a src/* output/sumac-wallpapers/contents/src/images
+mv output/sumac-wallpapers/contents/src/images/sumac-day-13.svg "output/sumac-wallpapers/contents/src/images/Sumac Day 13.svg"
+mv output/sumac-wallpapers/contents/src/images/sumac-night-13.svg "output/sumac-wallpapers/contents/src/images/Sumac Night 13.svg"
+ln -s "../src/images/Sumac Day 13.svg" output/sumac-wallpapers/contents/images/6000x6000.svg
+ln -s "../src/images/Sumac Night 13.svg" output/sumac-wallpapers/contents/images_dark/6000x6000.svg
 
 echo "Done"
