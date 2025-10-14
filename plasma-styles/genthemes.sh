d67679932b0bb3cad9d89da572491a57c98120d9
doncsugar <46570709+doncsugar@users.noreply.github.com>
2025-10-13 23:53:00 -0400

    Set base dialogs etc. to use translucent resources
    This was originally added because plasma hardcoded the use the of the dialog asset to the base assets rather than inheriting from translucent/. Thus, translucent resources were added to override base.

--- plasma-styles/genthemes.sh
+++ plasma-styles/genthemes.sh
@@ -19,23 +19,31 @@ echo "Making sumac-molten-glass-neutral-plasma"
 mkdir output/sumac-molten-glass-neutral-plasma
 
 cp -a src-molten-glass/* output/sumac-molten-glass-neutral-plasma
+# overwrite all base assets with translucent, there is only one style
+cp -a output/sumac-molten-glass-neutral-plasma/translucent/* output/sumac-molten-glass-neutral-plasma/
 
 echo "Making sumac-night-molten-glass-plasma"
 mkdir output/sumac-night-molten-glass-plasma
 
 cp -a src-molten-glass/* output/sumac-night-molten-glass-plasma
 cp -a sumac-night-molten-glass-plasma/* output/sumac-night-molten-glass-plasma
+# overwrite all base assets with translucent, there is only one style
+cp -a output/sumac-night-molten-glass-plasma/translucent/* output/sumac-night-molten-glass-plasma/
 
 echo "Making sumac-day-molten-glass-plasma"
 mkdir output/sumac-day-molten-glass-plasma
 
 cp -a src-molten-glass/* output/sumac-day-molten-glass-plasma
 cp -a sumac-day-molten-glass-plasma/* output/sumac-day-molten-glass-plasma
+# overwrite all base assets with translucent, there is only one style
+cp -a output/sumac-day-molten-glass-plasma/translucent/* output/sumac-day-molten-glass-plasma/
 
 echo "Making sumac-molten-glass-plasma-doncsugar"
 mkdir output/sumac-molten-glass-plasma-doncsugar
 
 cp -a src-molten-glass/* output/sumac-molten-glass-plasma-doncsugar
 cp -a sumac-molten-glass-plasma-doncsugar/* output/sumac-molten-glass-plasma-doncsugar
+# overwrite all base assets with translucent, there is only one style
+cp -a output/sumac-molten-glass-plasma-doncsugar/translucent/* output/sumac-molten-glass-plasma-doncsugar/
 
 echo "Done"
