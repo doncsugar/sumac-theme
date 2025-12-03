998c02fcefa791d7d51688334d0209f536be93e8
doncsugar <46570709+doncsugar@users.noreply.github.com>
2025-12-02 22:11:08 -0500

    Added molten glass icon index and script

--- icon-packs/genthemes.sh
+++ icon-packs/genthemes.sh
@@ -16,3 +16,18 @@ mkdir output/Sumac-Night
 cp -a src/* output/Sumac-Night
 cp -a Sumac-Night/* output/Sumac-Night
 $(cd output/Sumac-Night && find . -maxdepth 1 -mindepth 1 -type d | xargs -I % basename % | xargs -I % ln -sf "%" "%@2x")
+
+
+echo "Making Sumac-Day-Molten-Glass"
+mkdir output/Sumac-Day-Molten-Glass
+
+cp -a src/* output/Sumac-Day-Molten-Glass
+cp -a Sumac-Day-Molten-Glass/* output/Sumac-Day-Molten-Glass
+$(cd output/Sumac-Day-Molten-Glass && find . -maxdepth 1 -mindepth 1 -type d | xargs -I % basename % | xargs -I % ln -sf "%" "%@2x")
+
+echo "Making Sumac-Night-Molten-Glass"
+mkdir output/Sumac-Night-Molten-Glass
+
+cp -a src/* output/Sumac-Night-Molten-Glass
+cp -a Sumac-Night-Molten-Glass/* output/Sumac-Night-Molten-Glass
+$(cd output/Sumac-Night-Molten-Glass && find . -maxdepth 1 -mindepth 1 -type d | xargs -I % basename % | xargs -I % ln -sf "%" "%@2x")
