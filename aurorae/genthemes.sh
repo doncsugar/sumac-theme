55e68e50f4d481acfdd0e50218b0c030feba5c59
doncsugar <46570709+doncsugar@users.noreply.github.com>
2026-04-25 15:11:08 -0400

    added edge color themes to show content behind
    The lower opacity edge shows more color from behind the decoration

--- aurorae/genthemes.sh
+++ aurorae/genthemes.sh
@@ -10,12 +10,26 @@ cp -a sumac-molten-glass/src-molten-glass-day/* output/sumac-day-molten-glass-bl
 cp -a sumac-molten-glass/sumac-day-molten-glass-blur/* output/sumac-day-molten-glass-blur
 
 
+echo "Making sumac-day-molten-glass-blur-edge-color"
+mkdir output/sumac-day-molten-glass-blur-edge-color
+
+cp -a sumac-molten-glass/src-molten-glass-day/* output/sumac-day-molten-glass-blur-edge-color
+cp -a sumac-molten-glass/sumac-day-molten-glass-blur-edge-color/* output/sumac-day-molten-glass-blur-edge-color
+
+
+
 echo "Making sumac-night-molten-glass-blur"
 mkdir output/sumac-night-molten-glass-blur
 
 cp -a sumac-molten-glass/src-molten-glass-night/* output/sumac-night-molten-glass-blur
 cp -a sumac-molten-glass/sumac-night-molten-glass-blur/* output/sumac-night-molten-glass-blur
 
+
+echo "Making sumac-night-molten-glass-blur-edge-color"
+mkdir output/sumac-night-molten-glass-blur-edge-color
+
+cp -a sumac-molten-glass/src-molten-glass-night/* output/sumac-night-molten-glass-blur-edge-color
+cp -a sumac-molten-glass/sumac-night-molten-glass-blur-edge-color/* output/sumac-night-molten-glass-blur-edge-color
 
 
 echo "Making sumac-day-molten-glass-opaque"
